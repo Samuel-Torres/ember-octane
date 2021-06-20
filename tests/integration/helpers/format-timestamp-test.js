cc68dddc8ee9adefc0fad8a852851667d9258338
Samuel-Torres <rillatube@gmail.com>
2021-06-20 11:49:45 -0400

    Completed writing the routes for team and login. Updated the application.hbs to move that code to the team layer. Moving on to the next section

--- tests/integration/helpers/format-timestamp-test.js
+++ tests/integration/helpers/format-timestamp-test.js
@@ -8,10 +8,10 @@ module('Integration | Helper | format-timestamp', function(hooks) {
 
   // Replace this with your real tests.
   test('it renders', async function(assert) {
-    this.set('inputValue', '1234');
+    this.set('myDate', "05-20-1991");
 
-    await render(hbs`{{format-timestamp inputValue}}`);
+    await render(hbs`{{format-timestamp myDate}}`);
 
-    assert.equal(this.element.textContent.trim(), '1234');
+    assert.equal(this.element.textContent.trim(), 'May 20, 1991 00:00.00 AM');
   });
 });
